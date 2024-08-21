0ac4829ce224789a7d27251ec71d528dc49543f2
Juanjo Saez <jjsaez@ulp.edu.ar>
2024-08-20 21:08:40 -0300

    MODIFICACIONES VARIAS

--- src/p2ejercicio2/numero.java
+++ src/p2ejercicio2/numero.java
@@ -3,7 +3,11 @@ package p2ejercicio2;
 public class numero {
     int valor;
     String color="rojo";
-    int docena;
+    int docena; // y centena
+    
+    
+    
+    // HOLA GIT
     
     public numero(int n, int docena) {
         this.valor = n;
@@ -18,8 +22,7 @@ public class numero {
    boolean esPar(){
        return (valor%2==0);  //13%2 = 1 
    }
-   
-   private void setColor(){
+      private void setColor(){
    // la suma de sus cifras da un resultado impar: el 12 es compuesto de 1 y 2 que sumados dan 3 que es impar.   
     int n=valor,dig1, dig2;
     System.out.println("cifras sumadas: "+ (n%10+n/10));
